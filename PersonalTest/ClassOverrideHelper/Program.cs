59eb23b8c4a00c90767913c92a061394876b514d
guoshun.duan@itdev.xiangguang.com <guoshun.duan@itdev.xiangguang.com>
2018-08-29 11:16:17 +0800

    tj2

---
+++ PersonalTest/ClassOverrideHelper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassOverrideHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractOverriderClass aOC = new AbstractOverriderClass("我是独立思考者");
+            aOC.AbstractFunc();
+            aOC.VirtualFunc("我是自己");
+            AbstractBasicClass.NormalFunc();
+
+            NormalClass nC = new NormalClass();
+            IPrint print = new NormalClass();
+            IPrintOut printOut = new NormalClass();
+
+            print.Test();
+            print.PrintIn();
+
+            nC.Test();
+            nC.PrintIn();
+
+            printOut.Test();
+            printOut.Out();
+            Console.ReadKey();
+        }
+    }
+}
